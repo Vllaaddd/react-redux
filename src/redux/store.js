d41066c7165ff9bb6b0e5b587a0a9b9a9a6e6a6a
Vlad <vladikhoncharuk@gmail.com>
2024-01-23 20:49:48 +0200

    commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { taskReducer } from "./taskSlice";
 import { filterReducer } from "./filterSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { combineReducers, legacy_createStore as createStore } from "redux";
 
-export const store = configureStore({
-  reducer: {
-    tasks: taskReducer,
-    filters: filterReducer,
-  }
+const persistConfig = {
+  key: 'root',
+  storage
+}
+
+const rootReducer = combineReducers({
+  tasks: taskReducer,
+  filters: filterReducer,
 })
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+const persistedFilterReducer = persistReducer(persistConfig, filterReducer)
+
+
+export const store = createStore(persistedReducer)
+
+export const persistor = persistStore(store)
